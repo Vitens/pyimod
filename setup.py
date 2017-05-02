0c0ca10d1b50d7a8c39324eb331ca81dadf3bcce
Sjoerd Rijpkema <Sjoerd.Rijpkema@vitens.nl>
2017-05-02 15:39:10 +0200

    change discription

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='pyimod',
       version='0.1',
-      description='Vitens PI Wrapper',
+      description='iMOD files read, edit and write',
       url='https://github.com/VitensTC/pyimod/',
       author='Sjoerd Rijpkema',
       author_email='Sjoerd.Rijpkema@vitens.nl',
