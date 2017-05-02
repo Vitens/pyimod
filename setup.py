99b0473e878a4e8472c852236874ce33bebe8a75
Sjoerd Rijpkema <Sjoerd.Rijpkema@vitens.nl>
2017-05-02 16:10:53 +0200

    change setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='pyimod',
       version='0.1',
@@ -7,5 +7,6 @@ setup(name='pyimod',
       author='Sjoerd Rijpkema',
       author_email='Sjoerd.Rijpkema@vitens.nl',
       packages=['pyimod'],
-      install_requires=['pyshp'],
+      install_requires=['pyshp',\
+                        'numpy'],
       zip_safe=False)
