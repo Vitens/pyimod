4a48d0c576c0ccb3a990fa1f990c1f679c016d22
Sjoerd Rijpkema <Sjoerd.Rijpkema@vitens.nl>
2017-05-02 15:29:16 +0200

    remove metadata

--- pyimod/__init__.py
+++ pyimod/__init__.py
@@ -1,9 +1,1 @@
-#-------------------------------------------------------------------------------
-# Name:         iMOD-interpreter
-# Version:      2.0
-# Purpose:      read/write IDF-, IFF- and IPF-files
-# Author:       Sjoerd Rijpkema, Vitens
-# Created:      25-02-2015
-#-------------------------------------------------------------------------------
-
 import .iff, .ipf, .idf, .plugin
